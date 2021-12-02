c558501595c73ffa5f9f2c9a3757cf1b3ca7db8e
Matija Bertović <matbertovic@gmail.com>
2021-12-02 12:46:10 +0100

    add day 1 and day 2 solutions

---
+++ 2021/01/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+
+def read_file(path):
+    with open(path, "r") as f:
+        return [float(l.strip()) for l in f.readlines()]
+
+def cnt_increases_window(nums, wsize):
+    cnt = 0
+    for i, n in enumerate(nums[wsize:]):
+        if n > nums[i]:
+            cnt += 1
+
+    return cnt
+
+def main():
+    nums = read_file("input.txt")
+
+    solution_1 = cnt_increases_window(nums, 1)
+    solution_2 = cnt_increases_window(nums, 3)
+
+    print(f"Part 1: {solution_1}\n"
+          f"Part 2: {solution_2}")
+
+if __name__ == '__main__':
+    main()
---
+++ 2021/02/main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+
+def read_file(path):
+    with open(path, "r") as f:
+        lines = []
+        for l in f.readlines():
+            line = l.strip().split()
+            lines.append((line[0], float(line[1])))
+
+        return lines
+
+def final_position(commands, use_aim):
+    horizontal, depth, aim = 0, 0, 0
+    for d, x in commands:
+        if d == "forward":
+            horizontal += x
+            depth += aim * x
+        elif d == "down":
+            aim += x
+        else:
+            aim -= x
+
+    if not use_aim:
+        depth = aim
+
+    return horizontal * depth
+
+def main():
+    commands = read_file("input.txt")
+
+    solution_1 = final_position(commands, False)
+    solution_2 = final_position(commands, True)
+
+    print(f"Part 1: {solution_1}\n"
+          f"Part 2: {solution_2}")
+
+if __name__ == '__main__':
+    main()
