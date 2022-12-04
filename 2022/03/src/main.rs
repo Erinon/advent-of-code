dfd460666e595e9aaadd23164fd7ddbef8eed7d5
Matija Bertovic <matbertovic@gmail.com>
2022-12-04 23:47:48 +0100

    Add 2022 day 03 solutions

--- 2022/03/src/main.rs
+++ 2022/03/src/main.rs
@@ -1,14 +1,8 @@
-use std::io;
-
 mod part1;
 mod part2;
 
-fn read_stdin_lines() -> Vec<String> {
-    io::stdin().lines().collect::<Result<_, _>>().unwrap()
-}
-
 fn main() {
-    let in_lines = read_stdin_lines();
+    let in_lines: Vec<String> = std::io::stdin().lines().collect::<Result<_, _>>().unwrap();
 
     println!(
         "Day 3: Rucksack Reorganization\nPart One: {}\nPart Two: {}",
